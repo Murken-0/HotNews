ea6f0aaaeff5796c88d2f5b0a5b1b76df906dbf1
Vlad <muravievvlad.2002@gmail.com>
2020-10-08 12:18:21 +0300

    add

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotNews
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This app need to be improved by you");
+            Console.ReadLine();
+        }
+    }
+}
